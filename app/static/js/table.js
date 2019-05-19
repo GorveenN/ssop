90ef8bd7c943c728e6988de919cf94c505063899
Piotr Karpiński <gorveenn@gmail.com>
2019-05-19 20:37:42 +0200

    Added polish description to dataTables on search page.

--- app/static/js/table.js
+++ app/static/js/table.js
@@ -1,3 +1,28 @@
-$(document).ready( function () {
-    $('#table_id').DataTable();
-} );
+$(document).ready(function () {
+    $('#table_id').DataTable({
+        "language": {
+            "decimal": "",
+            "emptyTable": "Brak wynikow dla podanego zapytania",
+            "info": "Rekordy od _START_ do _END_ z _TOTAL_ wynikow.",
+            "infoEmpty": "Brak wynikow",
+            "infoFiltered": "(filtered from _MAX_ total entries)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Pokaz _MENU_ rekordow",
+            "loadingRecords": "Ladowanie...",
+            "processing": "Przetwarzanie...",
+            "search": "Wyszukaj:",
+            "zeroRecords": "Brak wynikow",
+            "paginate": {
+                "first": "Poczatek",
+                "last": "Koniec",
+                "next": "Dalej",
+                "previous": "Do tylu"
+            },
+            "aria": {
+                "sortAscending": ": aktywuj by sortowac rosnaco",
+                "sortDescending": ": aktywuj by sortowac malejaco"
+            }
+        }
+    });
+});
