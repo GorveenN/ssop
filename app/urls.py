cb24885d065bfff6c31e854bdd45c7c8a6604a9d
Jacek <Jecet4@wp.pl>
2019-05-20 19:34:01 +0200

    Added no-cache wrapper to subject page url

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
+from django.views.decorators.cache import never_cache
 
 urlpatterns = [
     path('',                        views.ssop_home,    name='ssop_home'),
@@ -10,7 +11,7 @@ urlpatterns = [
     path('teachers/<int:usos_id>/<str:subject>', views.teacher_comment_page, name='teacher_comment_page'),
     path('subjects/add_subject_comment', views.add_subject_comment, name='add_subject_comment'),
     path('subjects/add_subject_vote', views.add_subject_vote, name='add_subject_vote'),
-    path('subjects/<str:usos_id>', views.subject_page, name='subject_page'), # TODO problem: it shouldn't be a string, but
+    path('subjects/<str:usos_id>', never_cache(views.subject_page), name='subject_page'), # TODO problem: it shouldn't be a string, but
     # int obviously doesn't work and slug / uuid seems to fail as well :/
     path('user_rules/', views.rules_page,   name='rules_page'),
     path('report/',       views.report_comment, name='report_comment'),
